249c3f03707ad4f5e58b8516b2f796fef6835e47
alonsojj <juan13032007@gmail.com>
2025-04-06 01:40:02 -0300

    adicionado rotas dinamicas no projeto

--- src/app/Projetos/page.tsx
+++ src/app/Projetos/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Projeto {
   id: number;
@@ -17,15 +17,15 @@ export default function Projetos() {
   useEffect(() => {
     async function fetchProjetos() {
       try {
-        const res = await fetch("/api/projetos");
+        const res = await fetch('/api/projetos');
         if (!res.ok) {
-          throw new Error("Erro ao buscar os projetos");
+          throw new Error('Erro ao buscar os projetos');
         }
         const data = await res.json();
         setProjetos(data);
       } catch (error) {
         console.error(error);
-        setError("Não foi possível carregar os projetos.");
+        setError('Não foi possível carregar os projetos.');
       }
     }
 
@@ -34,22 +34,22 @@ export default function Projetos() {
 
   if (error) {
     return (
-      <div className="p-6">
-        <p className="text-red-500">{error}</p>
+      <div className='p-6'>
+        <p className='text-red-500'>{error}</p>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        <div className="p-6">
-          <h1 className="text-2xl font-bold mb-4">Projetos</h1>
+    <div className='flex min-h-screen flex-col'>
+      <main className='flex-grow'>
+        <div className='p-6'>
+          <h1 className='mb-4 text-2xl font-bold'>Projetos</h1>
           {projetos.map((projeto) => (
-            <Link key={projeto.id} href={`/projeto/${projeto.slug}`}>
-              <div className="bg-red-900 text-white p-4 rounded-lg hover:bg-red-800 cursor-pointer mb-4">
-                <h2 className="text-lg font-semibold">{projeto.title}</h2>
-                <p className="text-sm">{projeto.description}</p>
+            <Link key={projeto.id} href={`/projetos/${projeto.slug}`}>
+              <div className='mb-4 cursor-pointer rounded-lg bg-red-900 p-4 text-white hover:bg-red-800'>
+                <h2 className='text-lg font-semibold'>{projeto.title}</h2>
+                <p className='text-sm'>{projeto.description}</p>
               </div>
             </Link>
           ))}
